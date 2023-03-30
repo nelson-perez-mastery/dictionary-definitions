8f7a77d7076abeef9ad0461cb346e4401e78fb26
Nelson J Perez <nelson.perez@mastery.net>
2023-03-29 20:43:13 -0500

    Fix the replay functions

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -85,10 +85,10 @@ function loadDictionaryAsset() {
 
 loadDictionaryAsset();
 
-async function onGetEvent(data: any)       { Log.info({msg: "Sending GET data to kafka",       data}); }
-async function onInsertEvent(data: any)    { Log.info({msg: "Sending INSERT data to kafka",    data}); }
-async function onUpdateEvent(data: any)    { Log.info({msg: "Sending UPDATE data to kafka",    data}) };
-async function onDeleteEvent(data: any)    { Log.info({msg: "Sending DELETE data to kafka",    data}); }
+async function onGetEvent(data: any)       { Log.info({msg: "Sending GET data to kafka???",       data}); }
+async function onInsertEvent(data: any)    { Log.info({msg: "Sending INSERT data to kafka???",    data}); }
+async function onUpdateEvent(data: any)    { Log.info({msg: "Sending UPDATE data to kafka???",    data}) };
+async function onDeleteEvent(data: any)    { Log.info({msg: "Sending DELETE data to kafka???",    data}); }
 
 // Setup the web server
 const app = express();
@@ -124,19 +124,19 @@ function dictionarySetHandler(req: Request, res: Response) {
     const word = sanitize(req.params.word)
     const definition = req.params.definition;
     const response = dictionarySet(word, definition, false);
-    res.status(200).json(res).end();
+    res.status(200).json(response).end();
 }
 
 function dictionarySet(word: string, definition: string, replay: boolean) {
     const current = dictionaryMap.get(word);
-    const response = {success: true, addOrUpdate: ''};
+    const response = {success: true, insertOrUpdate: 'unknown'};
     var before: any = undefined;
     if(current) {
         before = {word, current}
 
         // Update the definition on the index
         dictionaryDefinitionIndex.update(current.idx, definition);
-        response.addOrUpdate = 'update';
+        response.insertOrUpdate = 'update';
         // Updating definition on the wrapper reference shared by the map and entries array
         current.definition = definition;
 
@@ -145,7 +145,7 @@ function dictionarySet(word: string, definition: string, replay: boolean) {
     }
     else {
         addWordDefinition(word, definition, dictionaryEntries.length);
-        response.addOrUpdate = 'add'
+        response.insertOrUpdate = 'insert'
         if(!replay) onInsertEvent({serverId, word, definition, response, after: {word, definition}});
     }
 
@@ -161,8 +161,8 @@ function dictionarySet(word: string, definition: string, replay: boolean) {
 app.get('/dictionary/delete/:word', dictionaryDeleteHandler);
 function dictionaryDeleteHandler(req: Request, res: Response) {
     const word = sanitize(req.params.word);
-    dictionaryDelete(word, false);
-    res.status(200).json({success: true}).end();
+    const response = dictionaryDelete(word, false);
+    res.status(200).json(response).end();
 }
 
 function dictionaryDelete(word: string, replay: boolean) {
@@ -176,174 +176,177 @@ function dictionaryDelete(word: string, replay: boolean) {
         dictionaryMap.delete(word);
     }
     if(!replay) onDeleteEvent({serverId, word, deleted: {word, definition: prev?.definition}});
+    return {success: true};
     // callOnDeleteHandlers() -> ex. update database, send kafka message, send notification to other nodes
 }
 
-/**
- * 
- * Dictionary Definition search handler
- * 
-*/
-app.get('/dictionary/search_definitions', dictionarySearcDefinitionshHandler);
-function dictionarySearcDefinitionshHandler(req: Request, res: Response) {
-    const query = sanitize(String(req.query.q));
+namespace Search {
+    /**
+     * 
+     * Dictionary Definition search handler
+     * 
+    */
+    app.get('/dictionary/search_definitions', dictionarySearcDefinitionshHandler);
+    function dictionarySearcDefinitionshHandler(req: Request, res: Response) {
+        const query = sanitize(String(req.query.q));
 
-    var limit = Number.parseInt(String(req.query.limit));
-    limit = isNaN(limit)? 25:limit;
+        var limit = Number.parseInt(String(req.query.limit));
+        limit = isNaN(limit)? 25:limit;
 
-    var offset = Number.parseInt(String(req.query.offset));
-    offset = isNaN(offset)? 0:offset;
+        var offset = Number.parseInt(String(req.query.offset));
+        offset = isNaN(offset)? 0:offset;
 
-    const suggest = String(req.query.suggest).toLocaleLowerCase().trim() !== 'false';
+        const suggest = String(req.query.suggest).toLocaleLowerCase().trim() !== 'false';
 
-    const resultIndexes = dictionaryDefinitionIndex.search(query, {limit, offset, suggest});
-    const results = resultIndexes.map((x: number) => dictionaryEntries[x]).map((x: [string, DefinitionWrapper]) => { return {word: x[0], definition: x[1].definition}});
-    res.status(200).json({query, limit, offset, suggest, results_length: results.length, results}).end();
-}
-
-/**
- * 
- * Dictionary Word search handler helper which is ranked by tokenizer of strict, forward, 
- * and full in that order.
- * 
- */
-function dictionarySearchWordsRandkedHandlerHelper(req: Request, res: Response, strict: boolean, forward: boolean, full: boolean, sort: boolean = true) {
-    const query = sanitize(String(req.query.q))//.split(' ');
-
-    var limit = Number.parseInt(String(req.query.limit));
-    limit = isNaN(limit)? 25:limit;
-
-    var offset = Number.parseInt(String(req.query.offset));
-    offset = isNaN(offset)? 0:offset;
-
-    const suggest = String(req.query.suggest).toLocaleLowerCase().trim() !== 'false';
-
-    const strictResults: {word: string, definition: string}[] = [];
-
-    const strictResultIndexes = strict ? dictionaryWordStrictIndex.search(query, {limit, offset, suggest}) : [];
-    for(const idx of strictResultIndexes) {
-        const [word, wrapper] = dictionaryEntries[idx];
-        const definition = wrapper.definition;
-        strictResults.push({word, definition})
-    }
-    if(sort) strictResults.sort((a, b) => a.word.length === b.word.length ? 0 : (a.word.length > b.word.length? 1 : -1));
-
-    if(strictResults.length >= limit) {
-        const results = strictResults
-        results.length = limit;
+        const resultIndexes = dictionaryDefinitionIndex.search(query, {limit, offset, suggest});
+        const results = resultIndexes.map((x: number) => dictionaryEntries[x]).map((x: [string, DefinitionWrapper]) => { return {word: x[0], definition: x[1].definition}});
         res.status(200).json({query, limit, offset, suggest, results_length: results.length, results}).end();
-        return;
     }
 
-    const forwardResults: {word: string, definition: string}[] = [];
-    const forwardResultIndexes = forward ? dictionaryWordForwardIndex.search(query, {limit, offset, suggest}) : [];
-    const strictIndexesSet = new Set<number>(strictResultIndexes);
-    for(let idx of forwardResultIndexes) {
-        if(strictIndexesSet.has(idx)) continue;
+    /**
+     * 
+     * Dictionary Word search handler helper which is ranked by tokenizer of strict, forward, 
+     * and full in that order.
+     * 
+     */
+    function dictionarySearchWordsRandkedHandlerHelper(req: Request, res: Response, strict: boolean, forward: boolean, full: boolean, sort: boolean = true) {
+        const query = sanitize(String(req.query.q))//.split(' ');
 
-        const [word, wrapper] = dictionaryEntries[idx];
-        forwardResults.push({word, definition: wrapper.definition});
-    }
-    if(sort) forwardResults.sort((a, b) => a.word.length === b.word.length ? 0 : (a.word.length > b.word.length? 1 : -1))
+        var limit = Number.parseInt(String(req.query.limit));
+        limit = isNaN(limit)? 25:limit;
 
-    if((strictResults.length + forwardResults.length) >= limit) {
-        const results = [...strictResults, ...forwardResults]
+        var offset = Number.parseInt(String(req.query.offset));
+        offset = isNaN(offset)? 0:offset;
+
+        const suggest = String(req.query.suggest).toLocaleLowerCase().trim() !== 'false';
+
+        const strictResults: {word: string, definition: string}[] = [];
+
+        const strictResultIndexes = strict ? dictionaryWordStrictIndex.search(query, {limit, offset, suggest}) : [];
+        for(const idx of strictResultIndexes) {
+            const [word, wrapper] = dictionaryEntries[idx];
+            const definition = wrapper.definition;
+            strictResults.push({word, definition})
+        }
+        if(sort) strictResults.sort((a, b) => a.word.length === b.word.length ? 0 : (a.word.length > b.word.length? 1 : -1));
+
+        if(strictResults.length >= limit) {
+            const results = strictResults
+            results.length = limit;
+            res.status(200).json({query, limit, offset, suggest, results_length: results.length, results}).end();
+            return;
+        }
+
+        const forwardResults: {word: string, definition: string}[] = [];
+        const forwardResultIndexes = forward ? dictionaryWordForwardIndex.search(query, {limit, offset, suggest}) : [];
+        const strictIndexesSet = new Set<number>(strictResultIndexes);
+        for(let idx of forwardResultIndexes) {
+            if(strictIndexesSet.has(idx)) continue;
+
+            const [word, wrapper] = dictionaryEntries[idx];
+            forwardResults.push({word, definition: wrapper.definition});
+        }
+        if(sort) forwardResults.sort((a, b) => a.word.length === b.word.length ? 0 : (a.word.length > b.word.length? 1 : -1))
+
+        if((strictResults.length + forwardResults.length) >= limit) {
+            const results = [...strictResults, ...forwardResults]
+            results.length = Math.min(results.length, limit);
+            res.status(200).json({query, limit, offset, suggest, results_length: results.length, results}).end();
+            return;
+        }
+        
+        const fullResults: {word: string, definition: string}[] = [];
+        const forwardResultIndexesSet = new Set<number>(forwardResultIndexes);
+        const fullResultIndexes = full ? dictionaryWordFullIndex.search(query, {limit, offset, suggest: true}) : [];
+        for(let idx of fullResultIndexes) {
+            if(forwardResultIndexesSet.has(idx) || strictIndexesSet.has(idx)) continue;
+
+            const [word, wrapper] = dictionaryEntries[idx];
+            fullResults.push({word, definition: wrapper.definition});
+        }
+
+        if(sort) fullResults.sort((a, b) => a.word.length === b.word.length ? 0 : (a.word.length > b.word.length? 1 : -1));
+
+        const results = [...strictResults, ...forwardResults, ...fullResults];
         results.length = Math.min(results.length, limit);
         res.status(200).json({query, limit, offset, suggest, results_length: results.length, results}).end();
-        return;
-    }
-    
-    const fullResults: {word: string, definition: string}[] = [];
-    const forwardResultIndexesSet = new Set<number>(forwardResultIndexes);
-    const fullResultIndexes = full ? dictionaryWordFullIndex.search(query, {limit, offset, suggest: true}) : [];
-    for(let idx of fullResultIndexes) {
-        if(forwardResultIndexesSet.has(idx) || strictIndexesSet.has(idx)) continue;
-
-        const [word, wrapper] = dictionaryEntries[idx];
-        fullResults.push({word, definition: wrapper.definition});
     }
 
-    if(sort) fullResults.sort((a, b) => a.word.length === b.word.length ? 0 : (a.word.length > b.word.length? 1 : -1));
-
-    const results = [...strictResults, ...forwardResults, ...fullResults];
-    results.length = Math.min(results.length, limit);
-    res.status(200).json({query, limit, offset, suggest, results_length: results.length, results}).end();
-}
-
-/**
- * 
- * Dictionary Word search handler which is ranked by first strict tokenized matches,
- * then forward tokenized matches and finally a full tokenized index matches.
- * 
-*/
-app.get('/dictionary/search_words', dictionarySearchWordsRandkedHandler);
-app.get('/dictionary/search_words_ranked', dictionarySearchWordsRandkedHandler);
-function dictionarySearchWordsRandkedHandler(req: Request, res: Response) {
-    dictionarySearchWordsRandkedHandlerHelper(req, res, true, true, true, true);
-}
-
-/**
- * Helper handler for the dictionary word search handlers
- */
-function searchWordHandlerHelper(req: Request, res: Response, strict: boolean, forward: boolean, full: boolean, sort: boolean = false): void {
-    const query = String(req.query.q);
-
-    var limit = Number.parseInt(String(req.query.limit));
-    limit = isNaN(limit)? 25 : limit;
-
-    var offset = Number.parseInt(String(req.query.offset));
-    offset = isNaN(offset)? 0:offset;
-
-    const suggest = (String(req.query.suggest).toLocaleLowerCase().trim() === 'false') ? false : true;
-
-    const preResults: {word: Word, wrapper: DefinitionWrapper}[] = [];
-
-    const strictResultIndexes: number[] = strict ? dictionaryWordStrictIndex.search(query, {limit, offset, suggest: true}): [];
-    const forwardResultIndexes: number[] = forward ? dictionaryWordForwardIndex.search(query, {limit, offset, suggest: true}): [];
-    const fullResultIndexes: number[] = full ? dictionaryWordFullIndex.search(query, {limit, offset, suggest}) : [];
-
-    const allIndexes = [...strictResultIndexes, ...forwardResultIndexes, ...fullResultIndexes]
-    const allIndexSet = new Set<number>(allIndexes);
-
-    for(let idx of allIndexSet) {
-        const [word, wrapper] = dictionaryEntries[idx];
-        preResults.push({word, wrapper});
+    /**
+     * 
+     * Dictionary Word search handler which is ranked by first strict tokenized matches,
+     * then forward tokenized matches and finally a full tokenized index matches.
+     * 
+    */
+    app.get('/dictionary/search_words', dictionarySearchWordsRandkedHandler);
+    app.get('/dictionary/search_words_ranked', dictionarySearchWordsRandkedHandler);
+    function dictionarySearchWordsRandkedHandler(req: Request, res: Response) {
+        dictionarySearchWordsRandkedHandlerHelper(req, res, true, true, true, true);
     }
 
-    if(sort) preResults.sort((a, b) => a.word.length === b.word.length ? 0 : (a.word.length > b.word.length? 1 : -1));
-    //preResults.length = Math.min(preResults.length, limit);
-    const results = preResults.map(({word, wrapper: {definition}}) => {return {word: word, definition}});
-    res.status(200).json({query, limit, offset, suggest, results_length: results.length, results}).end();
-}
+    /**
+     * Helper handler for the dictionary word search handlers
+     */
+    function searchWordHandlerHelper(req: Request, res: Response, strict: boolean, forward: boolean, full: boolean, sort: boolean = false): void {
+        const query = String(req.query.q);
 
-/**
- * 
- * Dictionary Word strict search only
- * 
-*/
-app.get('/dictionary/search_words_strict', dictionarySearchWordsHandlerStrict);
-function dictionarySearchWordsHandlerStrict(req: Request, res: Response) {
-    searchWordHandlerHelper(req, res, true, false, false);
-}
+        var limit = Number.parseInt(String(req.query.limit));
+        limit = isNaN(limit)? 25 : limit;
 
-/**
- * 
- * Forward word search only
- * 
-*/
-app.get('/dictionary/search_words_forward', dictionarySearchWordsHandlerForward);
-function dictionarySearchWordsHandlerForward(req: Request, res: Response) {
-    searchWordHandlerHelper(req, res, false, true, false);
-}
+        var offset = Number.parseInt(String(req.query.offset));
+        offset = isNaN(offset)? 0:offset;
 
-/**
- * 
- * Full word search only
- * 
-*/
-app.get('/dictionary/search_words_full', dictionarySearchWordsHandlerFull);
-function dictionarySearchWordsHandlerFull(req: Request, res: Response) {
-    searchWordHandlerHelper(req, res, false, false, true);
+        const suggest = (String(req.query.suggest).toLocaleLowerCase().trim() === 'false') ? false : true;
+
+        const preResults: {word: Word, wrapper: DefinitionWrapper}[] = [];
+
+        const strictResultIndexes: number[] = strict ? dictionaryWordStrictIndex.search(query, {limit, offset, suggest: true}): [];
+        const forwardResultIndexes: number[] = forward ? dictionaryWordForwardIndex.search(query, {limit, offset, suggest: true}): [];
+        const fullResultIndexes: number[] = full ? dictionaryWordFullIndex.search(query, {limit, offset, suggest}) : [];
+
+        const allIndexes = [...strictResultIndexes, ...forwardResultIndexes, ...fullResultIndexes]
+        const allIndexSet = new Set<number>(allIndexes);
+
+        for(let idx of allIndexSet) {
+            const [word, wrapper] = dictionaryEntries[idx];
+            preResults.push({word, wrapper});
+        }
+
+        if(sort) preResults.sort((a, b) => a.word.length === b.word.length ? 0 : (a.word.length > b.word.length? 1 : -1));
+        //preResults.length = Math.min(preResults.length, limit);
+        const results = preResults.map(({word, wrapper: {definition}}) => {return {word: word, definition}});
+        res.status(200).json({query, limit, offset, suggest, results_length: results.length, results}).end();
+    }
+
+    /**
+     * 
+     * Dictionary Word strict search only
+     * 
+    */
+    app.get('/dictionary/search_words_strict', dictionarySearchWordsHandlerStrict);
+    function dictionarySearchWordsHandlerStrict(req: Request, res: Response) {
+        searchWordHandlerHelper(req, res, true, false, false);
+    }
+
+    /**
+     * 
+     * Forward word search only
+     * 
+    */
+    app.get('/dictionary/search_words_forward', dictionarySearchWordsHandlerForward);
+    function dictionarySearchWordsHandlerForward(req: Request, res: Response) {
+        searchWordHandlerHelper(req, res, false, true, false);
+    }
+
+    /**
+     * 
+     * Full word search only
+     * 
+    */
+    app.get('/dictionary/search_words_full', dictionarySearchWordsHandlerFull);
+    function dictionarySearchWordsHandlerFull(req: Request, res: Response) {
+        searchWordHandlerHelper(req, res, false, false, true);
+    }
 }
 
 app.listen(8081);
